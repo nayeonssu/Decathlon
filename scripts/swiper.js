af51fbf1c3c2a64d4ed828d2b3af9de2cd092057
nayeonssu <sys7552@gmail.com>
2025-07-08 03:06:16 +0900

    데카트론 작업중

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -15,14 +15,31 @@ const bestSwiper = new Swiper('#best',{
 })
 /* ----------------------2행 */
 /* 2행,6행,7행 스와이퍼 */
-const newSwiper = new Swiper('.new_swiper', {
+const newSwiper = new Swiper('#new_swiper', {
     slidesPerView:'auto',
     spaceBetween:15,
     centeredSlides: false,
     slidesOffsetLeft: 70,
 })
-/* 반복 데이터 */
-const eventSwiper = new Swiper('.event_swiper', {
+const kidsSwiper = new Swiper('#kids_swiper', {
+    slidesPerView:'auto',
+    spaceBetween:15,
+    centeredSlides: false,
+    slidesOffsetLeft: 70,
+})
+const saleSwiper = new Swiper('#sale_swiper', {
+    slidesPerView:'auto',
+    spaceBetween:15,
+    centeredSlides: false,
+    slidesOffsetLeft: 70,
+})
+const summerSwiper = new Swiper('#summer_swiper', {
+    slidesPerView:'auto',
+    spaceBetween:15,
+    centeredSlides: false,
+    slidesOffsetLeft: 70,
+})
+const runnerSwiper = new Swiper('#runner_swiper', {
     slidesPerView:'auto',
     spaceBetween:15,
     centeredSlides: false,
