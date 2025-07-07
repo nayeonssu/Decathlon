94efbea54b4cdf064cfee1b04d0deb3d685150be
nayeonssu <sys7552@gmail.com>
2025-07-07 18:15:13 +0900

    작업중

--- scripts/DB.js
+++ scripts/DB.js
@@ -1,103 +1,505 @@
-/* DB.js */
-const productData = [
+/* 반복컬러 */
+/* const colorClass = ['black','white','blue','gray','red','green','purple','pink','beige','redbroun'] */
+
+/* ----------------------------------newDB */
+const newData = [
     {
-        color :[
-            "color_black.png",
-            "color_blue.png",
-            "color_blue.png",
-            "color_gray.png",
-            "color_red.png",
-            "color_white.png",
+        image : './img_new_001',
+        colors :[
+            "black",
+            "blue",
+            "blue",
+            "gray",
+            "red",
+            "white",
         ],
         brand : "DECATHLON",
         name : "남성 러닝 반팔 티 런 드라이 100",
+        originalPrice : '',
+        salePercent : '',
         price : 9900,
     },{
-        color :[
-            "color_white.png",
-            "color_black.png",
-            "color_blue.png",
+        image : './img_new_002',
+        colors :[
+            "white",
+            "black",
+            "blue",
         ],
         brand : "KALENJI",
         name : "남성 러닝 윈드 자켓 런 100",
+        originalPrice : '',
+        salePercent : '',
         price : 29900,
     },{
-        color :[
-            "color_black.png",
-            "color_white.png",
-            "color_red.png",
-            "color_green.png",
+        image : './img_new_003',
+        colors :[
+            "black",
+            "white",
+            "red",
+            "green",
         ],
         brand : "QUECHUA",
         name : "남성 등산 바람막이 자켓 헬륨 500",
+        originalPrice : '',
+        salePercent : '',
         price : 59900,
     },{
-        color :[
-            "color_gray.png",
-            "color_white.png",
-            "color_green.png",
+        image : './img_new_004',
+        colors :[
+            "gray",
+            "white",
+            "green",
         ],
         brand : "KALENJI",
         name : "남성 러닝 자외선 차단 반팔 티 런 드라이 500",
+        originalPrice : '',
+        salePercent : '',
         price : 15900,
     },{
-        color :[
-            "color_black.png",
-            "color_black.png",
-            "color_green.png",
-            "color_gray.png",
-            "color_blue.png",
-            "color_blue.png",
+        image : './img_new_005',
+        colors :[
+            "black",
+            "black",
+            "green",
+            "gray",
+            "blue",
+            "blue",
         ],
         brand : "KIPRUN",
         name : "남성 8인치 러닝 경량 쇼츠 런 드라이 플러스 500",
+        originalPrice : '',
+        salePercent : '',
         price : 21900,
     },{
-    color: [
-            "color_blue.png",
-            "color_white.png",
-            "color_gray.png"
+        image : './img_new_001',
+        colors: [
+            "blue",
+            "white",
+            "gray"
         ],
         brand: "DOMYOS",
         name: "남성 피트니스 반팔 티셔츠",
+        originalPrice : '',
+        salePercent : '',
         price: 12900,
     },{
-        color: [
-            "color_black.png",
-            "color_red.png"
+        image : './img_new_001',
+        colors: [
+            "black",
+            "red"
         ],
         brand: "QUECHUA",
         name: "남성 방수 등산 자켓 MH100",
+        originalPrice : '',
+        salePercent : '',
         price: 49900,
     },
-    {
-        color: [
-            "color_white.png",
-            "color_blue.png",
-            "color_green.png"
-        ],
-        brand: "KIPSTA",
-        name: "남성 축구 반팔 저지 F100",
-        price: 7900,
-    },
-    {
-        color: [
-            "color_gray.png",
-            "color_black.png"
-        ],
-        brand: "ARTENGO",
-        name: "남성 테니스 반팔 폴로셔츠",
-        price: 19900,
-    },
-    {
-        color: [
-            "color_red.png",
-            "color_white.png",
-            "color_blue.png",
-            "color_gray.png"
-        ],
-        brand: "INESIS",
-        name: "남성 골프 반팔 티셔츠 DRY",
-        price: 18900,
-    }
 ]
+
+/* --------------------------------- summserDB */
+const summerDataMan = [
+    {
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "white",
+            "white",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 반팔 래쉬가드 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 8900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 레깅스 수영복 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 24900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "black",
+        ],
+        brand : "NAVAIJI",
+        name : "남성 4부 수영 재머 레깅스 쇼츠 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 19900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "black",
+            "blue",
+            "black",
+            "black",
+            "black",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 래쉬가드 500",
+        originalPrice :'',
+        salePercent : '',
+        price : 24900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "white",
+            "white",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 반팔 래쉬가드 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 8900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "white",
+            "white",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 반팔 래쉬가드 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 8900,
+    },
+]
+
+/* --------------------------------- runningDB */
+const runningDataGirl = [
+    {
+        image : './img_kids_001',
+        colors :[
+            "black",
+            "purlpe",
+            "blue",
+            "green",
+            "purlpe",
+        ],
+        brand : "DOMYOS",
+        name : "스포츠 브라 하이 서포으 F900(내장 패드)",
+        originalPrice :'',
+        salePercent : '',
+        price : 24900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "greem",
+            "purple",
+            "beige",
+        ],
+        brand : "QUECHUA",
+        name : "여성 경량 등산 반팔 티 MH500",
+        originalPrice :'',
+        salePercent : '',
+        price : 19900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "pink",
+            "white",
+            "gray",
+            "black",
+        ],
+        brand : "KALENJI",
+        name : "여성 러닝 반팔 티 런 드라이 100",
+        originalPrice :9900,
+        salePercent : '30%',
+        price : 6900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "black",
+            "white",
+            "redbroun",
+            "green",
+        ],
+        brand : "KIPRUN",
+        name : "여성 러닝 숏 싱글렛 런 드라이 500",
+        originalPrice :'',
+        salePercent : '',
+        price : 24900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "white",
+            "white",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 반팔 래쉬가드 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 8900,
+    },{
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "white",
+            "white",
+            "blue",
+        ],
+        brand : "OLAIAN",
+        name : "남성 서핑 반팔 래쉬가드 100",
+        originalPrice :'',
+        salePercent : '',
+        price : 8900,
+    },
+]
+
+/* --------------------------------- kidsDB */
+const kidsDataGirl = [
+    {
+        image : './img_kids_001',
+        colors :[
+            "blue",
+            "black",
+            "purple",
+        ],
+        brand : "OLAIAN",
+        name : "여아 서핑 긴팔 래쉬가드 UV TOP500 ML",
+        originalPrice :22000,
+        salePercent : '55%',
+        price : 9900,
+    },{
+        image : './img_kids_002',
+        colors :[
+            "white",
+            "purple",
+        ],
+        brand : "QUECHUA",
+        name : "여아 등산 바람막이 자켓 헬륨 MH100",
+        originalPrice : '',
+        salePercent : '',
+        price : 29900,
+    },{
+        image : './img_kids_003',
+        colors :[
+            "black",
+        ],
+        brand : "OLAIAN",
+        name : "여아 서핑 반팔 래쉬가드 UV TOP100 MC",
+        originalPrice : '',
+        salePercent : '',
+        price : 14900,
+    },{
+        image : './img_kids_004',
+        colors :[
+            "blue",
+            "blue",
+            "white",
+        ],
+        brand : "OLAIANI",
+        name : "여아 X백 서핑 원피스 수영복 하날레이 100",
+        originalPrice : '',
+        salePercent : '',
+        price : 13900,
+    },{
+        image : './img_kids_005',
+        colors :[
+            "purple",
+        ],
+        brand : "OLAIANI",
+        name : "아동(여아) 긴팔 원피스 수영복 SUZY500",
+        originalPrice : '',
+        salePercent : '',
+        price : 39900,
+    },{
+        image : './img_kids_006',
+        colors :[
+            "black",
+            "blue",
+            "blue",
+            "gray",
+            "red",
+            "white",
+        ],
+        brand : "DECATHLON",
+        name : "남성 러닝 반팔 티 런 드라이 100",
+        originalPrice : '',
+        salePercent : '',
+        price : 9900,
+    },{
+        image : './img_kids_007',
+        colors :[
+            "black",
+            "blue",
+            "blue",
+            "gray",
+            "red",
+            "white",
+        ],
+        brand : "DECATHLON",
+        name : "남성 러닝 반팔 티 런 드라이 100",
+        originalPrice : '',
+        salePercent : '',
+        price : 9900,
+    },
+]
+
+/* --------------------------------- saleDB */
+const saleDataGirl = [
+    {
+        image : './img_sale_001.jpg',
+        colors :[
+            'pink',
+            'white',
+            'gray',
+            'black',
+        ],
+        brand : "KALENJI",
+        name : "여성 러닝 반팔 티 런 드라이 100",
+        originalPrice : 9900,
+        salePercent : '30%',
+        price : 6900,
+    },{
+        image : './img_sale_002.jpg',
+        colors :[
+            'purple',
+            'black'
+        ],
+        brand : "KIPRUN",
+        name : "여성 러닝 경량 싱글렛 런 900 라이트",
+        originalPrice : 34900,
+        salePercent : '14%',
+        price : 29900,
+    },{
+        image : './img_sale_003.jpg',
+        colors :[
+            'black'
+        ],
+        brand : "KIPRUN",
+        name : "여성 러닝 7부 레깅스 런 드라이 100",
+        originalPrice : 17900,
+        salePercent : '6%',
+        price : 19900,
+    },{
+        image : './img_sale_004.jpg',
+        colors :[
+            'beige',
+            'gray',
+            'purple',
+        ],
+        brand : "KIPRUN",
+        name : "여성 러닝 바지 런 드라이 500",
+        originalPrice : 33900,
+        salePercent : '25%',
+        price : 33900,
+    },{
+        image : './img_sale_005.jpg',
+        colors :[
+            'beige',
+            'gray',
+            'purple',
+        ],
+        brand : "KIPRUN",
+        name : "여성 러닝 자외선 차단 심리스 자켓 런 500",
+        originalPrice : 44900,
+        salePercent : '10%',
+        price : 35900,
+    },{
+        image : './img_sale_001.jpg',
+        colors :[
+            'beige',
+            'gray',
+            'purple',
+        ],
+        brand : "DECATHLON",
+        name : "남성 러닝 반팔 티 런 드라이 100",
+        originalPrice : '',
+        salePercent : '',
+        price : 9900,
+    },{
+        image : './img_sale_001.jpg',
+        colors :[
+            'beige',
+            'gray',
+            'purple',
+        ],
+        brand : "DECATHLON",
+        name : "남성 러닝 반팔 티 런 드라이 100",
+        originalPrice : '',
+        salePercent : '',
+        price : 9900,
+    },
+]
+
+/* -------------------------------- sale 반복문 */
+
+const salelist = document.querySelector("#sale .swiper-wrapper");/* html 찾기 */
+
+for (let i = 0; i < saleDataGirl.length; i++) {
+    const product = saleDataGirl[i];
+    console.log (product)
+}
+
+
+/* saleDataGirl.forEach(product => {
+    const li = document.createElement('li')li변수 생성
+    li.classList.add('swiper-slide')변수 li의 클래스이름 주기
+    console.log(li)
+    li.innerHTML = 
+    `
+    <a href="${product.href}">
+        <div class="pd_img">
+            <img src="./images/${product.image}" alt="${product.name}">
+            
+            <div class="colors">
+                <p class="${product.colors}"></p>
+            </div>
+
+            <button type="button" class="cart-btn">
+                <img src="./images/icon_cart_save.png" alt="장바구니 담기">
+            </button>
+        </div>
+
+        <div class="info">
+            <div class="top">
+                <p class="brand">${product.brand}</p>
+                <p class="name">${product.name}</p>
+            </div>
+            <div class="bottom">
+                <p class="original-price">${product.originalPrice}</p>
+                <p class="price">
+                    <span class="sale-percent">${product.salePercent}%</span>
+                    <span class="price">${product.price}</span>
+                </p>
+            </div>
+        </div>
+    </a>
+    `;
+    salelist.appendChild(li);
+}) */
+
+
+/* for문 활용 */
+/* const salelist = document.querySelector("#sale .swiper-wrapper");
+
+for (let i = 0; i < saleDataGirl.length; i++) {
+const product = saleDataGirl[i];
+
+const li = document.createElement("li");
+li.classList.add("swiper-slide");
+
+// 색상 span 요소 만들기
+let colorHTML = "";
+for (let j = 0; j < product.colors.length; j++) {
+colorHTML += `<span class="color-dot ${product.colors[j]}"></span>`;
+} */
