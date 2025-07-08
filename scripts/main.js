332af90155451bfbd23b1f840d33a0ce8f44ca11
nayeonssu <sys7552@gmail.com>
2025-07-09 01:26:02 +0900

    작업중

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /* main.js */
 /* 화면 위로 올라가는거 없애는방법 */
-const moreBtn = document.querySelector('section .more');
-const imgBtn = document.querySelector('.swiper-slide a');
+const moreBtn = document.querySelector('.more');
+const imgBtn = document.querySelector('.swiper-slide');
 
 moreBtn.addEventListener('click', function(e) {
   e.preventDefault(); // 페이지 위로 올라가는 기본 동작 막기
